87337e313b1073199ad4c86f03702457b707bec6
Kostiapolotsk <Kostiapolotsk@gmail.com>
2023-11-21 23:14:39 +0300

    Lesson3 исправление замечаний после ревью.

--- src/main/java/school/lesson3/Lesson_3.java
+++ src/main/java/school/lesson3/Lesson_3.java
@@ -28,39 +28,52 @@ public class Lesson_3 {
     }
 
     public static void main(String[] args) {
+        System.out.println("Задание №1:");
         System.out.println(check(2, 1));
-        isPlusOrMinus(2);
-        System.out.println(pozitivOrNegativ(2));
+        System.out.println("Задание №2:");
         stringInteger("Привет ", 5);
+        System.out.println("Задание №3:");
+        isPlusOrMinus(2);
+        System.out.println("Задание №4:");
+        System.out.println(pozitivOrNegativ(2));
+        System.out.println("Задание №5:");
         System.out.println(leapYear(2005));
-        arr(10, 8);
 
         //Задание №6
+        System.out.println("Задание №6:");
         int[] numbers = {1, 1, 0, 0, 1, 0, 1, 1, 0, 1};
         for (int i = 0; i < numbers.length; i++) {
             if (numbers[i] == 1) {
                 numbers[i] = 0;
             } else numbers[i] = 1;
-            //System.out.print(numbers[i]);
+            System.out.print(numbers[i] + " ");
         }
 
+        System.out.println(" ");
+
         //Задание №7
+        System.out.println("Задание №7:");
         int[] num = new int[100];
         for (int i = 0; i < num.length; i++) {
             num[i] = i + 1;
-            //System.out.println(num[i]);
+            System.out.print(num[i]);
         }
 
+        System.out.println(" ");
+
         //Задание №8
+        System.out.println("Задание №8:");
         int[] mas = {1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
         for (int i = 0; i < mas.length; i++) {
             if (mas[i] < 6) {
                 mas[i] = mas[i] * 2;
             }
-            //System.out.println(mas[i]);
+            System.out.print(mas[i]);
         }
+        System.out.println(" ");
 
         //Задание №9
+        System.out.println("Задание №9:");
         int[][] cube = new int[4][4];
         for (int i = 0; i < cube.length; i++) {
             cube[i][i] = 1;
@@ -70,6 +83,8 @@ public class Lesson_3 {
                 System.out.printf("%3s", cube[i][j]);
             System.out.println();
         }
+        System.out.println("Задание №10:");
+        arr(10,8);
     }
 
     //Задание №10
@@ -77,7 +92,7 @@ public class Lesson_3 {
         int arr2[] = new int[len];
         for (int i = 0; i < arr2.length; i++) {
             arr2[i] = initialValue;
-            //System.out.print(arr2[i]);
+            System.out.print(arr2[i]);
         }
         return arr2;
     }
